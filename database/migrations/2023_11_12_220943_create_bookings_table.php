79a7732746ca62b2027bc71c9fa63a87e4bb58d5
Ga-Jansu <gabrieljofig@gmail.com>
2023-11-12 19:18:34 -0300

    Adição Tabela Booking e Open_Schedules

---
+++ database/migrations/2023_11_12_220943_create_bookings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Enums\BookingStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_birthdayperson', 255);
+            $table->string('years_birthdayperson', 255);
+            $table->integer('qnt_invited'); 
+            $table->date('party_date', 0);
+            $table->foreignId('open_schedule_id')->constrained(
+                table: 'open_schedules', indexName: 'bookings_open_schedule_id'
+            );
+            $table->enum('status', array_column(BookingStatus::cases(), 'name'));
+            $table->foreignId('user_id')->constrained(
+                table: 'users', indexName: 'bookings_user_id'
+            );
+            $table->foreignId('food_id')->constrained(
+                table: 'food', indexName: 'bookings_food_id'
+            );
+            $table->float('price'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
