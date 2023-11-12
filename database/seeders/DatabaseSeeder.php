b67317f5e024d09804a226172984f2c633d76e65
Ga-Jansu <gabrieljofig@gmail.com>
2023-11-12 11:24:07 -0300

    Feature-Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Operacional',
+            'email' => 'ops@wagerfestas.com',
+            'email_verified_at' => now(),
+            "status" => "2",
+            'password' => '$2y$10$EcPDAt8.iqCXY/EqECnnnOYXSG1JUD7F1GrgWds00Bn0cO3yFUaCq', // password
+            'remember_token' => Str::random(10),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Administrativo',
+            'email' => 'admin@wagerfestas.com',
+            'email_verified_at' => now(),
+            "status" => "3",
+            'password' => '$2y$10$98zNPi/F4uBNO9Zl2nfCROPhBswEDIs0cc/N6ptsndtzx8dZW/vgC', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Comercial',
+            'email' => 'comer@wagerfestas.com',
+            'email_verified_at' => now(),
+            "status" => "1",
+            'password' => '$2y$10$02cvnCSgIVjaG2Y5lMZzOuRYdXWF9bhSVAwSF64imk43kRol0slNy', // password
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
