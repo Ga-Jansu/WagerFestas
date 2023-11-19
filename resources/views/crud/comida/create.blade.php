bb44a7856686a27dbd74ec3d7ac38cca67fb66c5
Itonsow <gustavoito223@gmail.com>
2023-11-19 17:37:37 -0300

    comida menu

--- resources/views/crud/comida/create.blade.php
+++ resources/views/crud/comida/create.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,18 +12,46 @@
 </head>
 
 <x-app-layout>
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <a href="/admin/comida" class="text-white hover:underline bg-red-500 text-white px-4 py-2 rounded-full mb-4 flex justify-center items-center mx-auto">Voltar</a>
-            <form action="{{ route('comida.create') }}" method="POST" class="flex flex-col justify-center items-center py-3">
-                @csrf()
-                <input type="text" placeholder="Assunto" name="name" class="mb-4">
-                <textarea name="body" id="body" cols="30" rows="10" placeholder="Comidas" class="mb-4"></textarea>
-                <textarea name="drink" id="drink" cols="30" rows="10" placeholder="Bebidas" class="mb-4"></textarea>
-                <input type="number" placeholder="Preço" name="money" class="mb-4">
-                <button type="submit" class="bg-green-500 text-white px-6 py-3 rounded-full">Enviar</button>
-            </form>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
+                <a href="/admin/comida"
+                    class="text-white hover:underline bg-red-500 text-white px-4 py-2 rounded-full mb-4 flex justify-center items-center mx-auto">Voltar</a>
+                <form action="{{ route('comida.create') }}" method="POST"
+                    class="w-full max-w-lg mx-auto bg-white p-8 rounded-md shadow-md flex flex-col items-start">
+                    @csrf()
+                    <div class="mb-4 w-full">
+                        <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Assunto</label>
+                        <input type="text" id="name" name="name"
+                            class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                    </div>
+                    <div class="mb-4 w-full">
+                        <label for="body" class="block text-gray-700 text-sm font-bold mb-2">Comidas</label>
+                        <textarea name="body" id="body"
+                            class="form-textarea resize-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            cols="30" rows="10" placeholder="Comidas"></textarea>
+                    </div>
+                    <div class="mb-4 w-full">
+                        <label for="drink" class="block text-gray-700 text-sm font-bold mb-2">Bebidas</label>
+                        <textarea name="drink" id="drink"
+                            class="form-textarea resize-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            cols="30" rows="10" placeholder="Bebidas"></textarea>
+                    </div>
+                    <div class="mb-4 w-full">
+                        <label for="money" class="block text-gray-700 text-sm font-bold mb-2">Preço</label>
+                        <input type="number" id="money" name="money"
+                            class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                    </div>
+                    <div class="w-full">
+                        <div class="text-center mt-4">
+                            <button type="submit"
+                                class="bg-green-500 text-white px-4 py-2 rounded-full w-full max-w-sm mx-auto text-lg">Enviar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </x-app-layout>
+
 </html>
